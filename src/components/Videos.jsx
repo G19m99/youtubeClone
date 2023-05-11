47435d2b6e44d26e4e28222fcc9bb44604e4ae2c
G19m99 <gershymenzer@gmail.com>
2023-05-10 21:10:43 -0400

    minor css change

--- src/components/Videos.jsx
+++ src/components/Videos.jsx
@@ -1,17 +1,17 @@
 import React from 'react'
-import {Stack, Box} from '@mui/material';
+import { Stack, Box } from '@mui/material';
 import VideoCard from './VideoCard';
 import ChannelCard from './ChannelCard';
 
-const Videos = ({videos, direction}) => {
+const Videos = ({ videos, direction }) => {
 
-  if(!videos?.length) return 'Loading...';
+  if (!videos?.length) return 'Loading...';
   return (
-    <Stack direction={direction || 'row'} flexWrap='wrap' justifyContent='start' gap={2}>
-      {videos.map((item, index)=>(
+    <Stack direction={direction || 'row'} flexWrap='wrap' justifyContent='flex-start' gap={2}>
+      {videos.map((item, index) => (
         <Box key={index}>
-          {item.id.videoId && <VideoCard video={item}/>}
-          {item.id.channelId && <ChannelCard channelDetail={item}/>}
+          {item.id.videoId && <VideoCard video={item} />}
+          {item.id.channelId && <ChannelCard channelDetail={item} />}
 
         </Box>
       ))}
--- src/utils/fetchFromApi.js
+++ src/utils/fetchFromApi.js
@@ -1,5 +1,3 @@
-import axios from 'axios';
-
 export const BASE_URL = 'https://youtube-v31.p.rapidapi.com';
 
 const options = {
